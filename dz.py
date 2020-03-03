d8667c84707432cf53426e1171eb4d4e515f5e19
myemail4ik <58517192+myemail4ik@users.noreply.github.com>
2020-03-03 19:40:44 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,16 @@
+import numpy
+
+def s(N):
+	s_out=0
+	for i in range(1,N):
+		s=(((-1)**i)*(1/(2**i)))
+		print('intermediate value: '+str(s))
+		s_out=s+s_out
+	return s_out+1
+
+try:
+	N=int(input())
+except ValueError:
+	print("ERR")
+
+print("Out value: "+str(s(N)))
